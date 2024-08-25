493820df2ed2d7a4d16e722414f451c00d03a9d9
Thrisha <thrishayermal@gmail.com>
2024-08-25 15:49:52 +0530

    last part

---
+++ frontend/src/context/AuthContex.jsx
@@ -0,0 +1,13 @@
+import { useState, createContext, useContext } from "react";
+
+export const AuthContex = createContext();
+
+export const useAuthContext = () => {
+    return useContext(AuthContex);
+}
+
+export const AuthContexProvider = ({children}) => {
+    const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null);
+
+    return <AuthContex.Provider value={{authUser, setAuthUser}}> {children} </AuthContex.Provider>;
+};
